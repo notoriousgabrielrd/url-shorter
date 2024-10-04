5b400e500ac342170038026d8c9e5cd7867a8c64
gabrielDellrado <grddourado@cosmobots.io>
2024-10-04 03:45:13 -0300

    feat: adding authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,49 +1,41 @@
-// src/controllers/authController.js
-
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { validationResult } from 'express-validator';
-import { AppDataSource } from '../data-source.js';
-import { User } from '../entities/User.js';
+import { createUser, findUserByEmail } from '../models/User.js';
 
 export const register = async (req, res) => {
-  // Validação dos dados
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
-  const { email, password } = req.body;
+  const { email, password, name } = req.body;
 
   try {
-    const userRepository = AppDataSource.getRepository(User);
-    let user = await userRepository.findOneBy({ email });
+    let user = await findUserByEmail(email);
 
     if (user) {
-      return res.status(400).json({ msg: 'Usuário já existe' });
+      return res.status(400).json({ msg: 'User already exists!' });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 12);
 
-    user = new User();
-    user.email = email;
-    user.password = hashedPassword;
+    user = await createUser({ name, email, password: hashedPassword });
 
-    await userRepository.save(user);
-
-    const payload = { userId: user.id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const payload = { user_id: user.user_id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '12h' });
 
     res.status(201).json({ token });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Erro no servidor');
+    res.status(500).send('Server error!');
   }
 };
 
 export const login = async (req, res) => {
-  // Validação dos dados
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
@@ -51,24 +43,23 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const userRepository = AppDataSource.getRepository(User);
-    const user = await userRepository.findOneBy({ email });
+    const user = await findUserByEmail(email);
 
     if (!user) {
-      return res.status(400).json({ msg: 'Credenciais inválidas' });
+      return res.status(400).json({ msg: 'Invalid credentials email or password!' });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ msg: 'Credenciais inválidas' });
+      return res.status(400).json({ msg: 'Invalid credentials email or password!' });
     }
 
-    const payload = { userId: user.id };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const payload = { user_id: user.user_id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '12h' });
 
     res.json({ token });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Erro no servidor');
+    res.status(500).send('Server error');
   }
 };
